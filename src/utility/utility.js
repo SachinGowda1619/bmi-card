92a7dd8ed8696318d210b044d219c36c9926f30d
Sachin Gowda <99198081+SachinGowda1619@users.noreply.github.com>
2025-06-19 20:13:34 +0530

    test commit

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -9,7 +9,7 @@ const appData = {
     website: 'fitnesslounge.in',
     traineesData: {
         '8088616230': {
-            name: 'Sachin Sachin',
+            name: 'Sachin Test',
             age: 27,
             weight: 87.5,
             height: 182,
