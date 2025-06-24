aba0b7095e024a83933f9634329577b49ad2e13d
Sachin Gowda <99198081+SachinGowda1619@users.noreply.github.com>
2025-06-24 13:37:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './components/Home';
+import Home from './components/home';
 import QrCode from './components/QrCode';
+import Admin from './components/Admin';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/bmi-card" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/qrCode" element={<QrCode />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
   );
